8a1989d7c30b5c908eeaac510acc90f0e2a3a6df
Krish-Motisariya <161847733+Krish-Motisariya@users.noreply.github.com>
2024-07-05 05:49:28 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3) //we are using node js, not browser
+
+console.log(3+3);
+
+console.log("krish");
+
+let name=Krish
+let age=18
+let isLoggedIn=false
+
+//number => 2 to power 53
+//bigint
+//string=>""
+//boolean=>true/false
+//null=>standalone value
+//undefined
+//symbol=>unique
+
+//object
